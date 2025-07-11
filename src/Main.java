3da460f5e1ff098cb44e20b7874eb8794fe1f3e5
Violet <56974126+zhenyatevo@users.noreply.github.com>
2025-07-11 14:59:57 +0300

    code after exam

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,14 @@ public class Main {
         System.out.println("=== Пример 1: температура 27°C, влажность 45% ===");
         double temp1 = 27.0;
         double hum1 = 45.0;
-        double speed1 = controller.calculateFanSpeed(temp1, hum1);
+        controller.calculateFanSpeed(temp1, hum1);
 
 
         // Пример 2: температура 32°C, влажность 60%
         //System.out.println("\n\n=== Пример 2: температура 32°C, влажность 60% ===");
         //double temp2 = 17.0;
         //double hum2 = 60.0;
-        //double speed2 = controller.calculateFanSpeed(temp2, hum2);
+        //controller.calculateFanSpeed(temp2, hum2);
 
     }
 }
